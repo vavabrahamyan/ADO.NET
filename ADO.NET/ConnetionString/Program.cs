6566e3ef3577c0af7da4d68d5414480ab2fe5687
Vahe Abrahamyan <vavabrahamyan@gmail.com>
2020-02-17 15:59:14 +0400

    Connection with Data Base, Async connection

---
+++ ADO.NET/ConnetionString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Configuration;
+
+namespace ConnetionString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source= DESKTOP-BG4E31L; 
+                                        Internal Catalog= usersdb;
+                                        Integrate Security= True";
+
+            //string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            //Console.WriteLine(connectionString);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ADO.NET/SqlConnection/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace ADO.NET.Connection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source= DESKTOP-BG4E31L; Initial Catalog=usersdb; Integrated Security=True";
+            //ConnectionMethod(connectionString);
+            //ConnectWithDB(connectionString);
+
+            Console.ReadLine();
+        }
+        private static async Task ConnectWithDB(string connectionString)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+                Console.WriteLine("Async Connection is open");
+            }
+            Console.WriteLine("Async Connection is closed");
+        }
+
+        private static void ConnectionMethod(string connectionString)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Connection is open");
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Error 4004 {0}", ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+                Console.WriteLine("Connection is closed");
+            }
+
+            Console.Read();
+        }
+    }
+}
