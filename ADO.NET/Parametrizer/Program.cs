045fd71a5ddf6b62f30eb7faea4534ea0cdbd0b4
Vahe Abrahamyan <vavabrahamyan@gmail.com>
2020-02-20 18:56:31 +0400

    Parametrize

---
+++ ADO.NET/Parametrizer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Parametrizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=DESKTOP-BG4E31L;Initial Catalog=usersdb;Integrated Security=True";
+            int age = 23;
+            string name = "Kenny";
+            string sqlExpression = "INSERT INTO Users (Name, Age) VALUES (@name, @age);SET @id=SCOPE_IDENTITY()";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                SqlParameter nameParam = new SqlParameter("@name", name);
+                command.Parameters.Add(nameParam);
+                SqlParameter ageParam = new SqlParameter("@age", age);
+                command.Parameters.Add(ageParam);
+                SqlParameter idParam = new SqlParameter
+                {
+                    ParameterName = "@id",
+                    SqlDbType = SqlDbType.Int,
+                    Direction = ParameterDirection.Output 
+                };
+                command.Parameters.Add(idParam);
+
+                command.ExecuteNonQuery();
+
+                Console.WriteLine("New object Id: {0}", idParam.Value);
+            }
+        }
+    }
+}
--- ADO.NET/SDReader/Program.cs
+++ ADO.NET/SDReader/Program.cs
@@ -8,28 +8,18 @@ namespace SDReader
         static void Main(string[] args)
         {
             string connectionString = @"Data Source=DESKTOP-BG4E31L;Initial Catalog=usersdb;Integrated Security=True";
-            string sqlExtention = "SELECT * FROM Users";
+            string sqlExtention = "SELECT SUM(*) FROM Users";
 
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
                 SqlCommand command = new SqlCommand(sqlExtention, connection);
-                SqlDataReader reader = command.ExecuteReader();
 
-                if (reader.HasRows)
-                {
-                    Console.WriteLine($"{reader.GetName(0)}\t{reader.GetName(1)}\t{reader.GetName(2)}");
+                command.CommandText = "SELECT SUM(--columeName--) FROM --table--";
+                object num = command.ExecuteScalar();
 
-                    while (reader.Read())
-                    {
-                        object id = reader.GetValue(0);
-                        object name = reader.GetValue(1);
-                        object age = reader.GetValue(2);
 
-                        Console.WriteLine($"{id}\t{name}\t{age}");
-                    }
-                }
-                reader.Close();
+                Console.WriteLine(num);
             }
             Console.Read();
         }
